9436130421d9e5069d456c3caff8912b32ff4f6a
sumithprabhu <sumithprabhu07@gmail.com>
2024-01-08 16:59:09 +0530

    custom errors added

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -29,11 +29,21 @@ pragma solidity ^0.8.18;
  * @author Sumith Prabhu
  */
 contract Raffle {
+    error Raffle__NotEnoughEntranceFee();
+
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
-    function enterRaffle() public payable{
+
+    function enterRaffle() external payable{
+        if(msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEntranceFee();
+        }
+
+        s_players.push(payable(msg.sender));
 
     }
 
